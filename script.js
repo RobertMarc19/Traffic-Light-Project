1a5ed57c156460e99846572139d481a181ec85ea
RobertMarc19 <mogyorosi.robert08@gmail.com>
2024-10-31 12:37:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function changeColors() {
     let button = document.getElementById("lights");
     const colors = ["btn-danger", "btn-warning", "btn-success"];
     
-		let currentColor = colors.findIndex(colors => button.classList.contains(colors));
+    let currentColor = colors.findIndex(colors => button.classList.contains(colors));
     button.classList.remove(colors[currentColor]);
     
 		let nextColor = (currentColor + 1) % colors.length;
